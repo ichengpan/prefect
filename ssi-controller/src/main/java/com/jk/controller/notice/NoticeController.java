0baed74200478a2c0dc44d52ac8664d264b63a4f
chengpan <377822835@qq.com>
2017-08-16 17:34:52 +0800

    测试分支

---
+++ ssi-controller/src/main/java/com/jk/controller/notice/NoticeController.java
@@ -0,0 +1,10 @@
+package com.jk.controller.notice;
+
+/**
+ * Created by admin on 2017/8/16.
+ */
+public class NoticeController {
+    public static void main(String[] args) {
+        System.out.println("这是测试");
+    }
+}
